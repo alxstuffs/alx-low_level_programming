b77353566e1186bd1a5add9c4a3d7d34725d264f
alxstuffs <alxstuffs@proton.me>
2023-03-16 07:48:58 -0700

    Fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,10 @@
 int
 main(void)
 {
-	const char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char text[] = "and that piece of art is useful\" - Dora Korpar,",
+		text2[] = " 2015-10-19\n";
+
 	fwrite(text, 1, strlen(text), stderr);
+	fwrite(text2, 1, strlen(text2), stderr);
 	return (1);
 }
