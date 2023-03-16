42631b23f9cf36d8703e2679ab8d1f719056c014
alxstuffs <alxstuffs@proton.me>
2023-03-16 07:43:47 -0700

    Add source file for task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int
+main(void)
+{
+	const char text[] = "and that piece of art is useful\" - Dora Korpar,"
+		"2015-10-19\n";
+	fwrite(text, 1, strlen(text), stderr);
+	return 0;
+}
